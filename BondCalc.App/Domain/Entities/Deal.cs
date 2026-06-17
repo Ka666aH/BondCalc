0565b498e2ed12a67d6741b93868e9c485f14ee4
Ka666aH <dmitryhoroshev05@mail.ru>
2026-06-17 09:49:04 +0300

    Создать сущность сделки.

---
+++ BondCalc.App/Domain/Entities/Deal.cs
@@ -0,0 +1,9 @@
+﻿namespace BondCalc.App.Domain.Entities
+{
+    public class Deal
+    {
+        public double Price { get; init;  }
+        public double ACI { get; init; }
+        public DateOnly Date { get; init; }
+    }
+}
