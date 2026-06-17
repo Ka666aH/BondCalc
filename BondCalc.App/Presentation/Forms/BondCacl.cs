856896dca5cbf5f08c0d54bc1172962836cc8a6b
Ka666aH <dmitryhoroshev05@mail.ru>
2026-06-17 13:33:34 +0300

    Создать форму.

---
+++ BondCalc.App/Presentation/Forms/BondCacl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BondCalc.App.Presentation.Forms
+{
+    public partial class BondCacl : Form
+    {
+        public BondCacl()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- BondCalc.App/Program.cs
+++ BondCalc.App/Program.cs
@@ -1,3 +1,5 @@
+using BondCalc.App.Presentation.Forms;
+
 namespace BondCalc.App
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace BondCalc.App
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            System.Windows.Forms.Application.Run(new BondCacl());
         }
     }
 }
