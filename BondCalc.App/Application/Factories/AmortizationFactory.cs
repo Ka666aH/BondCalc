4694fddf3d590920fdece08b803e48307fe5fbe9
Ka666aH <dmitryhoroshev05@mail.ru>
2026-06-17 14:43:06 +0300

    Удалить фабрики.

--- BondCalc.App/Application/Factories/AmortizationFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using BondCalc.App.Domain.Entities;
-
-namespace BondCalc.App.Application.Factories
-{
-    public static class AmortizationFactory
-    {
-        public static Amortization Create(double amount, DateOnly date) => new(amount, date);
-    }
-}
--- BondCalc.App/Application/Factories/DealFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using BondCalc.App.Domain.Entities;
-
-namespace BondCalc.App.Application.Factories
-{
-    public static class DealFactory
-    {
-        public static Deal Create(double price, double aci, DateOnly date) => new(price, aci, date);
-    }
-}
